696c6a59ed515abc9b846709a949b04b7ce3f6d6
MMarco94 <marangoni.marco@femastudios.com>
2019-03-06 23:41:31 +0100

    Bugfix; Improved Sequence.takeWhileIncluive

--- src/main/kotlin/com/fmg/SequenceUtils.kt
+++ src/main/kotlin/com/fmg/SequenceUtils.kt
@@ -2,11 +2,27 @@ package com.fmg
 
 fun <T> Sequence<T>.shuffled() = toList().shuffled().asSequence()
 
+/**
+ * @return a sequence that ends when the given predicate becomes false. The first element that doesn't match the predicate is returned
+ */
 fun <T> Sequence<T>.takeWhileInclusive(predicate: (T) -> Boolean): Sequence<T> {
-    var shouldContinue = true
-    return takeWhile {
-        val result = shouldContinue
-        shouldContinue = predicate(it)
-        result
+    val wrapped = this.iterator()
+    return object : Sequence<T> {
+        private var shouldContinue = true
+
+        override fun iterator(): Iterator<T> {
+            return object : Iterator<T> {
+                override fun hasNext() = shouldContinue && wrapped.hasNext()
+
+                override fun next(): T {
+                    val ret = wrapped.next()
+                    if (!predicate(ret)) {
+                        shouldContinue = false
+                    }
+                    return ret
+                }
+            }
+        }
+
     }
 }
