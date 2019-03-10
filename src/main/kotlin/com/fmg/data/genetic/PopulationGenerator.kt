145c293d18eb7ce76217ee8fff91ef174de038af
Unknown <filippomaganza@gmail.com>
2019-03-10 12:35:52 +0100

    Small comment

--- src/main/kotlin/com/fmg/data/genetic/PopulationGenerator.kt
+++ src/main/kotlin/com/fmg/data/genetic/PopulationGenerator.kt
@@ -2,11 +2,15 @@ package com.fmg.data.genetic
 
 import com.fmg.data.Board
 import com.fmg.data.BoardGenerator
+import com.fmg.data.OneQueenPerRowRandomBoardGenerator
 
 interface PopulationGenerator {
     fun generatePopulation(): Collection<Board>
 }
 
+/**
+ * This PopulationGenerator keeps the number of board generated and the way in which they are generated as separate concepts.
+ */
 class IndependentPopulationGenerator(val boardSize: Int, val boardGenerator: BoardGenerator, val populationSize: Int) :
     PopulationGenerator {
     override fun generatePopulation(): Collection<Board> {
